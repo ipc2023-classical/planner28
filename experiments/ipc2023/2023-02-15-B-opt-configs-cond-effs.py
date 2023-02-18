9bf8a547bbe5bd32799055d25ad5619243ad47e6
Silvan Sievers <silvan.sievers@unibas.ch>
2023-02-18 10:21:54 +0100

    add cond-eff exp

---
+++ experiments/ipc2023/2023-02-15-B-opt-configs-cond-effs.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python
+
+import os
+from pathlib import Path
+import shutil
+
+from downward import suites
+from downward.cached_revision import CachedFastDownwardRevision
+from downward.experiment import (
+    _DownwardAlgorithm,
+    _get_solver_resource_name,
+    FastDownwardRun,
+)
+from lab.experiment import Experiment, get_default_data_dir
+
+from labreports import PerTaskComparison
+
+import project
+
+
+REPO = project.get_repo_base()
+BENCHMARKS_DIR = '../../benchmarks'
+SCP_LOGIN = "infai"
+REMOTE_REPOS_DIR = "/infai/myuser/repos"
+#SUITE = ["depot:p01.pddl", "grid:prob01.pddl", "gripper:prob01.pddl"]
+SUITE = ["miconic-simpleadl-adl:0-s1-0.pddl"]
+try:
+    REVISION_CACHE = Path(os.environ["DOWNWARD_REVISION_CACHE"])
+except KeyError:
+    REVISION_CACHE = Path(get_default_data_dir()) / "revision-cache"
+if project.REMOTE:
+    ENV = project.BaselSlurmEnvironment(email="my.name@myhost.ch", memory_per_cpu="9G")
+    SUITE = project.SUITE_STRIPS_AND_ADL
+else:
+    ENV = project.LocalEnvironment(processes=2)
+
+CONFIGS = [
+    ("blind", ["--search", "astar(blind())"]),
+    ("h2", ["--search", "astar(hm(m=2))"]),
+    ("hmax", ["--search", "astar(hmax())"]),
+    # M&S heuristics
+    ("mas-ssc-dfp-10s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[goal_relevance(), dfp(), total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=10))"]),
+    ("mas-ssc-sbmiasm-10s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[sf_miasm(shrink_strategy=shrink_bisimulation(greedy=false),max_states=50000,threshold_before_merge=1),total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=10))"]),
+    ("mas-ssc-dfp-60s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[goal_relevance(), dfp(), total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=60))"]),
+    ("mas-ssc-sbmiasm-60s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[sf_miasm(shrink_strategy=shrink_bisimulation(greedy=false),max_states=50000,threshold_before_merge=1),total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=60))"]),
+    ("mas-ssc-dfp-300s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[goal_relevance(), dfp(), total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=300))"]),
+    ("mas-ssc-sbmiasm-300s", ["--search", "astar(merge_and_shrink(shrink_strategy=shrink_bisimulation(greedy=false), merge_strategy=merge_sccs(order_of_sccs=topological, merge_selector=score_based_filtering(scoring_functions=[sf_miasm(shrink_strategy=shrink_bisimulation(greedy=false),max_states=50000,threshold_before_merge=1),total_order(atomic_ts_order=reverse_level,product_ts_order=new_to_old,atomic_before_product=false)])), label_reduction=exact(before_shrinking=true, before_merging=false), max_states=50k, threshold_before_merge=1, main_loop_max_time=300))"]),
+    # Operator-counting heuristics
+    ("hplus-relaxed", ["--search", "astar(operatorcounting([delete_relaxation_constraints(use_time_vars=false, use_integer_vars=false)]))"]),
+    ("hplus", ["--search", "astar(operatorcounting([delete_relaxation_constraints(use_time_vars=true, use_integer_vars=true)]))"]),
+]
+BUILD_OPTIONS = []
+DRIVER_OPTIONS = [
+    "--validate",
+    # Higher time limits probably don't make sense since we're building sequential portfolios.
+    # Maybe go higher for the cond-eff experiment, though, since there are fewer configurations.
+    "--overall-time-limit", "10m",
+    # Same memory limit as in competition.
+    "--overall-memory-limit", "8G",
+]
+# Pairs of revision identifier and revision nick.
+REVS = [
+    ("2deb3d9f2", ""),
+]
+ATTRIBUTES = [
+    "error",
+    "run_dir",
+    "search_start_time",
+    "search_start_memory",
+    "score_total_time",
+    "total_time",
+    "coverage",
+    "expansions",
+    "expansions_until_last_jump",
+    "memory",
+]
+
+exp = Experiment(environment=ENV)
+for rev, rev_nick in REVS:
+    cached_rev = CachedFastDownwardRevision(REPO, rev, BUILD_OPTIONS)
+    cached_rev.cache(REVISION_CACHE)
+    cache_path = REVISION_CACHE / cached_rev.name
+    dest_path = Path(f"code-{cached_rev.name}")
+    exp.add_resource("", cache_path, dest_path)
+    # Overwrite the script to set an environment variable.
+    exp.add_resource(
+        _get_solver_resource_name(cached_rev),
+        cache_path / "fast-downward.py",
+        dest_path / "fast-downward.py",
+    )
+    for config_nick, config in CONFIGS:
+        algo_name = f"{rev_nick}:{config_nick}" if rev_nick else config_nick
+
+        for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
+            algo = _DownwardAlgorithm(
+                algo_name,
+                cached_rev,
+                DRIVER_OPTIONS,
+                config,
+            )
+            run = FastDownwardRun(exp, algo, task)
+            exp.add_run(run)
+
+exp.add_parser(project.FastDownwardExperiment.EXITCODE_PARSER)
+exp.add_parser(project.FastDownwardExperiment.TRANSLATOR_PARSER)
+exp.add_parser(project.FastDownwardExperiment.SINGLE_SEARCH_PARSER)
+exp.add_parser(project.DIR / "parser.py")
+exp.add_parser(project.FastDownwardExperiment.PLANNER_PARSER)
+
+exp.add_step("build", exp.build)
+exp.add_step("start", exp.start_runs)
+exp.add_fetcher(name="fetch")
+
+if not project.REMOTE:
+    exp.add_step("remove-eval-dir", shutil.rmtree, exp.eval_dir, ignore_errors=True)
+    project.add_scp_step(exp, SCP_LOGIN, REMOTE_REPOS_DIR)
+
+project.add_absolute_report(
+    exp,
+    attributes=ATTRIBUTES,
+    # filter=[project.group_domains],
+)
+
+exp.run_steps()
