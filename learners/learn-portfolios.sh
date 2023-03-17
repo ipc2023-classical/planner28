971331951fe973d8a88a7bfdc3fedbf187b237c3
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-17 21:35:24 +0100

    Compress properties.

--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SATISFICING_DATA=../experiments/ipc2023/data/03-sat-configs-eval/properties-hardest.json
+SATISFICING_DATA=../experiments/ipc2023/data/03-sat-configs-eval/properties-hardest.json.xz
 
 # OPTIMAL
 ./stonesoup.py --track opt ../experiments/ipc2023/data/01-opt-configs-no-cond-effs-eval/properties-hardest.json.xz 110 | tee stonesoup-opt-strips.txt
