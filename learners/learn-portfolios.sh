7984c5d87cd84f52413b0999a65136c018cba952
Jendrik Seipp <jendrik.seipp@liu.se>
2023-03-17 09:26:47 +0100

    Add logs.

--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -2,8 +2,17 @@
 
 set -euo pipefail
 
-./stonesoup.py --track opt ~/projects/Downward/ipc-2023/fdss/experiments/ipc2023/data/01-opt-configs-no-cond-effs-eval/properties.xz 110 | tee stonesoup-opt-strips.txt
-./stonesoup.py --track opt ~/projects/Downward/ipc-2023/fdss/experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties.xz 120 | tee stonesoup-opt-cond-effs.txt
+#./stonesoup.py --track opt ../experiments/ipc2023/data/01-opt-configs-no-cond-effs-eval/properties-hardest.json.xz 110 | tee stonesoup-opt-strips.txt
+#./stonesoup.py --track opt ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz 120 | tee stonesoup-opt-cond-effs.txt
+#./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz --track opt | tee greedy-opt-strips.txt
+#./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz --track opt | tee greedy-opt-cond-effs.txt
 
-./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties.xz --track opt | tee greedy-opt-strips.txt
-./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties.xz --track opt | tee greedy-opt-cond-effs.txt
+SATISFICING_DATA=../experiments/ipc2023/data/03-sat-configs-eval/properties-hardest.json
+
+#./batch-stonesoup.sh ${SATISFICING_DATA} sat 1800 | tee batch-stonesoup-sat.txt
+./stonesoup.py --track sat ${SATISFICING_DATA} 30 | tee stonesoup-sat.txt
+#./greedy.py ${SATISFICING_DATA} --track sat | tee greedy-sat.txt
+
+#./batch-stonesoup.sh ${SATISFICING_DATA} agl 1800 | tee batch-stonesoup-agl.txt  # use sat portfolio
+#./stonesoup.py --track agl ${SATISFICING_DATA} 30 | tee stonesoup-agl.txt  # use sat portfolio
+./greedy.py ${SATISFICING_DATA} --track agl | tee greedy-agl.txt
