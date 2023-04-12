0e1e46a6019c92b8bb00b7c046ff94cf20e78e77
Jendrik Seipp <jendrik.seipp@liu.se>
2023-04-12 09:46:55 +0200

    Fix logfile.

--- learners/learn-portfolios.sh
+++ learners/learn-portfolios.sh
@@ -7,7 +7,7 @@ SATISFICING_DATA=../experiments/ipc2023/data/03-sat-configs-eval/properties-hard
 # OPTIMAL
 ./stonesoup.py --track opt ../experiments/ipc2023/data/01-opt-configs-no-cond-effs-eval/properties-hardest.json.xz 110 | tee stonesoup-opt-strips.txt
 ./stonesoup.py --track opt ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz 120 | tee stonesoup-opt-cond-effs.txt
-./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz --track opt | tee greedy-opt-strips.txt
+./greedy.py ../experiments/ipc2023/data/01-opt-configs-no-cond-effs-eval/properties-hardest.json.xz --track opt | tee greedy-opt-strips.txt
 ./greedy.py ../experiments/ipc2023/data/02-opt-configs-cond-effs-eval/properties-hardest.json.xz --track opt | tee greedy-opt-cond-effs.txt
 
 # SATISFICING
